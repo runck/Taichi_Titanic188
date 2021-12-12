8f7d47f5e3296597817750498f6481b7c9403b6a
Tiger <longhutiger@qq.com>
2021-12-12 22:38:16 +0800

    first

--- README.md
+++ README.md
@@ -1,109 +1,66 @@
-# 太极图形课S1-大作业
+# 太极图形课S1-大作业 188行代码的《泰坦尼克号》
 
 ## 作业来源
-> 请你介绍大作业的灵感来源，可以是自己原创的想法，也可以是复现一篇论文。如果有参考论文、文章或者参考代码，请**务必**给出来源。
+所谓来源，是一件可近可远的事情。
 
-## 运行方式
-> 展示项目运行方式，让更多的人在自己的机器上运行你的代码查看运行效果。
-> 为了复现运行效果，请给出你的运行环境。
-> 如果项目有依赖，请给出项目的依赖。
+从近，是其躯体，是流体小作业的改进，是基于Taichi自带示例的mpm，其丝滑仿若高山流水。从远，是其灵魂，是某篇[“99行代码的《冰雪奇缘》”](https://zhuanlan.zhihu.com/p/97700605)，其配图让人食欲大开。
 
-#### 运行环境：
-> 请列出`Taichi`程序运行环境，可以在命令行中输入命令：`$ ti`，然后复制输出的第一行。
+那时，当我第一次点开这段“冰雪奇缘”的视频，我看见了三块真空中栩栩如生的方块。于是，我以为我大概似乎也许的确是被骗进来了。
 
-#### 运行：
-> 请列出哪些代码可以运行以及如何运行，如果有命令行参数可以列出参数以及参数对应的含义。
+而这一骗，便从金秋到了寒冬。
 
-## 效果展示
-> 这里可以展示这份作业运行起来后的可视化效果，可以让其他人更直观感受到你的工作。可以是**图片**（比如渲染效果）、**动图**或**视频**。
+在这段时间里，我每天添一点知识，终于在这最后领会到了这寥寥数行后所包裹的精妙与恢弘，正像简若黑白又裹挟一切的太极。
 
-## 整体结构
-> 脉络清晰的结构能完整展示你的设计思想，以及实现方式，方便读者快速代入。Python的代码，可以使用命令：`yapf -i xx.py`来格式化。可以在repo的目录中包含如下内容：
-```
--LICENSE
--|data
--README.MD
--xx.py
-```
+而今，这一切又似乎即将回归来时的沉寂，仿佛随处可见的数值耗散，将一切殆尽。为了让这个故事从指数衰减的趋势重回太极般循环的螺旋，这次或许轮到我，来尝试把下一个人也骗进来。
 
-## 实现细节：
-> 请给出代码的具体实现流程和细节，让感兴趣的人能够更深入的了解你的代码。
+这大抵就是这个作业——188行代码的《泰坦尼克号》——的来源。
 
-# 示例
-下面给出一个模版供大家参考，在你提交大作业的时候可以删除示例部分。按上面的要求填写自己大作业的内容。
-
-## 作业来源
-布料仿真是计算机图形学中一个重要的仿真场景，在电影和游戏中都有很广泛的应用。一个很经典的布料仿真方法是 [*Large Steps in Cloth Simulation.*](https://www.cs.cmu.edu/~baraff/papers/sig98.pdf) David Baraff, Andrew Witkin. Siggraph 1998. 这也是皮克斯 (Pixar) 动画仿真器 **Fizt** 使用的布料仿真方法。在SCA 2020中有有一篇[论文](https://www.tkim.graphics/FEMBW/)描述了该论文中布料仿真模型对应的FEM形式。
-
-本次大作业就是对Baraff-Witkin布料仿真的实现。在实现过程中，可以了解能量、力和刚度矩阵的计算。除此之外，还可以了解Taichi语言中稀疏矩阵 (Sparse Matrix) 的使用，共轭梯度法 (Conjugate Gradient, CG) 的实现。
 
 ## 运行方式
 #### 运行环境：
-`[Taichi] version 0.8.7, llvm 10.0.0, commit 1c3c705a, osx, python 3.8.8`
+`[Taichi] version 0.8.5, llvm 10.0.0, commit 45c6ad48, win, python 3.9.1`
 
-#### 运行：
-在运行 `implicit_mass_spring_system.py`时，可以通过命令行参数 `-cg` 来控制是否使用 **CG** solver。
-
-- 使用 Direct solver:
-`python implicit_mass_spring_system.py`
-
-- 使用 CG solver:
-`python implicit_mass_spring_system.py  -cg`
+#### 运行方式：
+python Titanic188.py
 
 
 ## 效果展示
-左侧：Direct solver；右侧： Conjugate Gradient (CG) solver。 
-![mass spring demo](./data/direct_vs_cg.gif)
+#### 文字描述：
+1912年4月14日，星期天晚上，号称“永不沉没的”泰坦尼克号飞驰在它生命中最后的一个夜晚，驶向波涛汹涌中的死亡冰山……
+（免责声明：危险动作，请勿模仿；本电影为仿真模拟中，没有人员或动物受到伤害）
+
+
+#### 动图展示：
+![demo.gif](./data/demo.gif)
+
 
 ## 整体结构
 ```
 -LICENSE
 -|data
 -README.MD
--implicit_mass_spring_system.py
+-Titanic188.py
 ```
 
-## 实现细节：
-`implicit_mass_spring_system.py`是项目源代码，其中包含一个Cloth类和一个`main`函数。具体公式推导，可以看: [here](https://github.com/taichiCourse01/taichiCourse01/blob/main/material/09_implicit_integration.pdf) (P45-46)。
+
+## 实现细节
+
+### 主要效果
+基于Taichi自带示例的mpm法，补充实现了下列效果：
+
+1. 船，具有“水能载舟、亦能覆舟”的特性。载舟的实现方式，是通过统计其附近网格的水的总质量（因为浮力等于被排挤的水的重量）；覆舟的实现方式，是通过判断局部的水是否处于该局部船的上方。为了实现这一目标，目前将原来的网格按物质进行了拆分（或许有更省资源的方法？）。
+2. 冰山，一座冰的山状物，其实是“冰雪奇缘”的雪被爱莎施加了变硬魔法后得到。
+3. 爆炸物，把塑形形变中的箝位值设得更严格一些（尤其是压缩的塑形阈值），就能实现偶然的爆炸效果。其实或许是真爆炸——“数值爆炸”。这在别的代码里可能是bug，但请牢记，这在本剧组里是为了实现游轮漏油爆炸而特意留下的“特性”。
+
 
 ### 整体流程
-1. 布料的初始化
-2. 创建一个GUI来显示布料
-3. 根据命令行选项更新布料，默认使用 direct solver 来更新布料。可以命令行参数 `-cg` 来使用 CG solver。
-4. 在GUI中显示布料
+1. 起。创建船、水、冰山，并将其放置于具有视觉张力的非平衡初态。
+2. 承。让船在水面拍打、疾驰，溅起水花，撞向冰山。每一dt，都是动量于网格的暗中交易，也潜藏者浮力和重力从中作梗。
+3. 转。爆炸，到处爆炸，不可预料（包括作者与上帝）的却恰到好处的爆炸。
+4. 合。沉船，船因塑形变形失去了棱角，最终沉沦于冷酷的冰山脚下。
 
-### 布料类
-1. [初始化](https://github.com/FantasyVR/taichi_course_final_project/blob/304a32dfa686862adcb54f737ed6970e21fe8d5b/implicit_mass_spring.py#L13)
-   - 设置field，确定数据结构
-   - 初始化field，设置位置，速度，质量和每个弹簧两个端点索引
 
-2. 布料更新 Direct Solver: [`update_direct(h)`](https://github.com/FantasyVR/taichi_course_final_project/blob/8f79e0026237e75ec3abe7d09b39be0a2fadc994/implicit_mass_spring.py#L156)
+感谢观看！
 
-    **计算力**: [`compute_force()`](https://github.com/FantasyVR/taichi_course_final_project/blob/304a32dfa686862adcb54f737ed6970e21fe8d5b/implicit_mass_spring.py#L98)
-     - 重力
-     - 弹簧力
-     - 约束力: 固定布料的两个顶点，使用一个刚度极大的弹簧来模拟。
-     
-    **计算力的导数**:[`compute_force_Jacobians`](https://github.com/FantasyVR/taichi_course_final_project/blob/304a32dfa686862adcb54f737ed6970e21fe8d5b/implicit_mass_spring.py#L120)
-     - 弹簧力的导数
-     - 约束力的导数
-     
-    **组装刚度矩阵**: [`assemble_K`](https://github.com/FantasyVR/taichi_course_final_project/blob/304a32dfa686862adcb54f737ed6970e21fe8d5b/implicit_mass_spring.py#L138)
-     - 遍历每个弹簧，组建度矩阵
-     
-    **组装系统矩阵**：[here](https://github.com/FantasyVR/taichi_course_final_project/blob/304a32dfa686862adcb54f737ed6970e21fe8d5b/implicit_mass_spring.py#L162)
-     - 系统矩阵<img src="https://user-images.githubusercontent.com/6712304/145008587-696452f3-497e-4b6e-985d-2f7f8720110a.png" width="100">
-     
-    **计算系统矩阵右侧b**: [here](https://github.com/FantasyVR/taichi_course_final_project/blob/304a32dfa686862adcb54f737ed6970e21fe8d5b/implicit_mass_spring.py#L169)
-     - <img src="https://user-images.githubusercontent.com/6712304/145009103-ac8720e1-61ec-4dda-9121-820a62376316.png" width="150">
 
-    **求解线性系统**: [here](https://github.com/FantasyVR/taichi_course_final_project/blob/304a32dfa686862adcb54f737ed6970e21fe8d5b/implicit_mass_spring.py#L171)
-     - <img src="https://user-images.githubusercontent.com/6712304/145009271-67a7d412-eebd-41f8-9fbf-a22eb8f12bba.png" width="100">
-
-    **更新速度和位置**: [`directUpdatePosVel`](https://github.com/FantasyVR/taichi_course_final_project/blob/8f79e0026237e75ec3abe7d09b39be0a2fadc994/implicit_mass_spring.py#L151)
-     - <img src="https://user-images.githubusercontent.com/6712304/145009488-40d3a2d2-5d0c-454c-b654-235d94ae724d.png" width="200">
-     
-3. 布料更新 CG solver: [`update_cg(h)`](https://github.com/FantasyVR/taichi_course_final_project/blob/8f79e0026237e75ec3abe7d09b39be0a2fadc994/implicit_mass_spring.py#L249)
-
-   在使用CG进行布料更新的时候，同样需要计算力和计算力的导数。但我们使用CG的时候，不需要显式的构建出刚度矩阵，只需要计算矩阵-向量乘就可以。具体算法请参考：[here](https://github.com/taichiCourse01/taichiCourse01/blob/main/material/09_implicit_integration.pdf) (P105-110)。
 
---
+++ Titanic188.py
@@ -0,0 +1,213 @@
+import taichi as ti
+
+ti.init(arch=ti.gpu, debug=True)
+
+n_material = 3
+n_particle = ti.Vector.field(n_material, int, ())
+n_particle_ = [6000, 1000, 6000]
+n_particles = sum(n_particle_)
+n_grid = 128
+
+
+dx, inv_dx = 1 / n_grid, float(n_grid)
+dt = 5e-5
+
+grid_m = ti.field(float, (n_grid, n_grid, n_material))
+grid_v = ti.Vector.field(2, float, (n_grid, n_grid, n_material))
+grid_doff = ti.Vector.field(2, float, (n_grid, n_grid, n_material))
+
+
+m = ti.field(dtype=float, shape=n_material)  # mass 
+
+x = ti.Vector.field(2, dtype=float, shape=n_particles)  # position
+v = ti.Vector.field(2, dtype=float, shape=n_particles)  # velocity
+C = ti.Matrix.field(2, 2, dtype=float, shape=n_particles)  # affine velocity field
+F = ti.Matrix.field(2, 2, dtype=float, shape=n_particles)  # deformation gradient
+Jp = ti.field(dtype=float, shape=n_particles)  # plastic 
+material = ti.field(dtype=int, shape=n_particles)  # material id
+
+
+p_vol = (dx * 0.5)**2
+E, nu = 5e3, 0.2  # Young's modulus and Poisson's ratio
+mu_0, lambda_0 = E / (2 * (1 + nu)), E * nu / (
+    (1 + nu) * (1 - 2 * nu))  # Lame parameters
+
+touch_freeze_1_3=ti.field(ti.i32, ())
+touch_freeze_1_3[None]=False
+
+acc = ti.Vector.field(2, dtype=float, shape=())
+
+
+@ti.kernel
+def substep():
+    for i, j, k in grid_m:
+        grid_v[i, j, k] = [0, 0]
+        grid_m[i, j, k] = 0
+        
+    for p in x:
+        k = material[p]
+        base = (x[p] * inv_dx - 0.5).cast(int)
+        fx = x[p] * inv_dx - base.cast(float)
+        w = [0.5 * (1.5 - fx)**2, 0.75 - (fx - 1)**2, 0.5 * (fx - 0.5)**2]
+        F[p] = (ti.Matrix.identity(float, 2) + dt * C[p]) @ F[p]
+        h = max(0.1, min(5, ti.exp(10 * (1.0 - Jp[p]))))
+        mu, la = mu_0 * h, lambda_0 * h
+        if k == 0:
+            mu = 0.0
+        U, sig, V = ti.svd(F[p])
+        J = 1.0
+        for d in ti.static(range(2)):
+            new_sig = sig[d, d]
+            if k == 2:  # ice
+                new_sig = min(max(sig[d, d], 1 - 2.5e-1),
+                              1 + 4.5e-1)  # Plasticity
+            if k == 1:  # boat
+                new_sig = min(max(sig[d, d], 1 - 2.5e-2),
+                              1 + 4.5e-2)  # Plasticity   
+            Jp[p] *= sig[d, d] / new_sig
+            sig[d, d] = new_sig
+            J *= new_sig
+        if k == 0:
+            F[p] = ti.Matrix.identity(float, 2) * ti.sqrt(J)
+        elif k==1 or k==2:
+            F[p] = U @ sig @ V.transpose()
+        stress = 2 * mu * (F[p] - U @ V.transpose()) @ F[p].transpose(
+        ) + ti.Matrix.identity(float, 2) * la * J * (J - 1)
+        stress = (-dt * p_vol * 4 * inv_dx * inv_dx) * stress
+        affine = stress + m[k] * C[p]
+        
+        for i, j in ti.static(ti.ndrange(3, 3)):
+            offset = ti.Vector([i, j])
+            dpos = (offset.cast(float) - fx) * dx
+            weight = w[i][0] * w[j][1]
+            grid_v[base + offset, k] += weight * (m[k] * v[p] + affine @ dpos)
+            grid_m[base + offset, k] += weight * m[k]
+            grid_doff[base + offset, k] += -(offset.cast(float) - fx)
+
+    
+    for i, j in ti.ndrange(n_grid, n_grid):
+        if grid_m[i, j, 1]>0:
+            grid_v[i, j, 1] += acc[None] * dt * 3e-3
+            
+        if grid_m[i, j, 2]>0 and grid_m[i, j, 1]>0:
+            grid_v[i, j, 2], grid_v[i, j, 1] = \
+                grid_v[i, j, 2]*0.7 +grid_v[i, j, 1]*0.3, \
+                grid_v[i, j, 2]*0.3 +grid_v[i, j, 1]*0.7
+    
+        if grid_m[i, j, 1]>0 and grid_m[i, j, 0]>0:
+            grid_v[i, j, 1], grid_v[i, j, 0] = \
+                grid_v[i, j, 1]*0.99 + grid_v[i, j, 0]*0.01, \
+                grid_v[i, j, 1]*0.01 + grid_v[i, j, 0]*0.99
+            if not grid_doff[i, j, 0].y > grid_doff[i, j, 1].y:
+                grid_v[i, j, 1].y += grid_m[i, j, 0] * dt *1e2
+
+        if grid_m[i, j, 2]>0 and grid_m[i, j, 0]>0:
+            grid_v[i, j, 2], grid_v[i, j, 0] = \
+                grid_v[i, j, 2]*0.99 + grid_v[i, j, 0]*0.01, \
+                grid_v[i, j, 2]*0.01 + grid_v[i, j, 0]*0.99
+
+
+
+
+        
+    for i, j, k in grid_m:
+        if grid_m[i, j, k] > 0:
+            grid_v[i, j, k] = (1 / grid_m[i, j, k]) * grid_v[i, j, k]  # Momentum to velocity
+            
+            if k==2:
+                grid_v[i, j, k][1] += dt * -1 * 50
+            else:
+                grid_v[i, j, k][1] += dt * -1 * 30
+
+            if i < 3 and grid_v[i, j, k][0] < 0:
+                grid_v[i, j, k][0] = 0  # Boundary conditions
+            if i > n_grid - 3 and grid_v[i, j, k][0] > 0: grid_v[i, j, k][0] = 0
+            if j < 3 and grid_v[i, j, k][1] < 0: grid_v[i, j, k][1] = 0
+            if j > n_grid - 3 and grid_v[i, j, k][1] > 0: grid_v[i, j, k][1] = 0        
+        
+    for p in x:  # grid to particle (G2P)
+        pad=3.0/128
+        k = material[p]
+        base = (x[p] * inv_dx - 0.5).cast(int)
+        fx = x[p] * inv_dx - base.cast(float)
+        w = [0.5 * (1.5 - fx)**2, 0.75 - (fx - 1.0)**2, 0.5 * (fx - 0.5)**2]
+        new_v = ti.Vector.zero(float, 2)
+        new_C = ti.Matrix.zero(float, 2, 2)
+        
+        for i, j in ti.static(ti.ndrange(3, 3)):
+            dpos = ti.Vector([i, j]).cast(float) - fx
+            g_v = grid_v[base + ti.Vector([i, j]), k]
+            weight = w[i][0] * w[j][1]
+            new_v += weight * g_v
+            new_C += 4 * inv_dx * weight * g_v.outer_product(dpos)
+        v[p], C[p] = new_v, new_C
+        x[p] += dt * v[p]  # advection
+        pad=3.0/128
+        if x[p].x<pad: x[p].x=pad
+        if x[p].x>1-pad: x[p].x=1-pad
+        if x[p].y<pad: x[p].y=pad
+        if x[p].y>1-pad: x[p].y=1-pad
+
+
+@ti.kernel
+def init():
+    n_particle[None] = n_particle_
+    n_sum = 0
+    
+    m[0] = p_vol * 1.0
+    m[1] = p_vol * 1.0
+    m[2] = p_vol * 1.0
+    
+    for k in ti.static(range(n_material)):
+        for j in range(n_particle[None][k]):
+            i = n_sum+j
+            
+            material[i] = k
+            v[i] = [0, 0]
+            F[i] = ti.Matrix([[1, 0], [0, 1]])
+            C[i] = ti.Matrix.zero(float, 2, 2)
+            Jp[i] = 1   
+
+            if k==0: # water
+                x_ = ti.Vector([ti.random(), ti.random()])
+                x_y = x_.y * ti.sqrt((1-x_.x))
+                x[i] = [0.05 + x_.x*0.9, 0.05 + x_y*0.2]
+            if k==1: # boat
+                x_ = ti.Vector([ti.random()-0.5, ti.random()])
+                x_x = x_.x * ti.sqrt((x_.y+1))
+                x[i] = [0.2 + x_x*0.15, 0.22 + x_.y*0.04]
+                v[i] = [4, -1]
+            if k==2: # ice
+                x_ = ti.Vector([ti.random()-0.5, ti.random()])
+                x_x = x_.x * ti.sqrt((1-x_.y))
+                x[i] = [0.65 + x_x*0.2, 0 + x_.y*0.3]
+            
+        n_sum+=n_particle[None][k]
+
+
+
+gui = ti.GUI("Titanic188", res=512, background_color=0x112F41)
+init()
+
+tick=0
+while True:
+    if gui.get_event(ti.GUI.PRESS):
+        if gui.event.key == 'r': init()
+        elif gui.event.key in [ti.GUI.ESCAPE, ti.GUI.EXIT]: break
+    if gui.event is not None: acc[None] = [0, 0]
+    if gui.is_pressed(ti.GUI.LEFT, 'a'): acc[None][0] = -1
+    if gui.is_pressed(ti.GUI.RIGHT, 'd'): acc[None][0] = 1
+    if gui.is_pressed(ti.GUI.UP, 'w'): acc[None][1] = 2e-1
+    if gui.is_pressed(ti.GUI.DOWN, 's'): acc[None][1] = -2e-1
+    
+    for s in range(int(2e-3// dt)):
+        substep()
+    gui.circles(x.to_numpy(),
+                radius=1.5,
+                palette=[0x068587, 0xED553B, 0xEEEEF0, 0xA6B5F7, 0x3255A7, 0x6D35CB, 0xFE2E44, 0x26A5A7, 0xEDE53B],
+                palette_indices=material
+                )
+    gui.show()
+    # tick+=1
+    # if tick%5 ==1:
+        # gui.show(f'img/{tick:0>3d}.png')
